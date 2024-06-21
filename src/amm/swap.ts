84b6ab08da88e8a0b27e7cebeaec332aa542e7b3
Cruz Shia <iamahan0720@gmail.com>
2024-06-21 17:35:02 +0800

    amm swap support devnet

--- src/amm/swap.ts
+++ src/amm/swap.ts
@@ -1,4 +1,4 @@
-import { ApiV3PoolInfoStandardItem, fetchMultipleInfo } from '@raydium-io/raydium-sdk-v2'
+import { ApiV3PoolInfoStandardItem, toAmmComputePoolInfo, AmmV4Keys } from '@raydium-io/raydium-sdk-v2'
 import { initSdk, txVersion } from '../config'
 import BN from 'bn.js'
 import { isValidAmm } from './utils'
@@ -10,33 +10,42 @@ export const swap = async () => {
   const inputMint = '4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R' // RAY
   const poolId = '6UmmUiYoBjSrhakAobJw8BvkmJtDVxaeBtbt7rxWo1mg' // RAY-USDC pool
 
-  // note: api doesn't support get devnet pool info
-  const data = await raydium.api.fetchPoolById({ ids: poolId })
-  const poolInfo = data[0] as ApiV3PoolInfoStandardItem
+  let poolInfo: ApiV3PoolInfoStandardItem | undefined
+
+  let poolKeys: AmmV4Keys | undefined
+  let rpcData = await raydium.liquidity.getRpcPoolInfo(poolId)
+  let [baseReserve, quoteReserve, status] = [rpcData.baseReserve, rpcData.quoteReserve, rpcData.status.toNumber()]
+
+  if (raydium.cluster === 'mainnet') {
+    // note: api doesn't support get devnet pool info, so in devnet else we go rpc method
+    const data = await raydium.api.fetchPoolById({ ids: poolId })
+    poolInfo = data[0] as ApiV3PoolInfoStandardItem
+    if (!isValidAmm(poolInfo.programId)) throw new Error('target pool is not AMM pool')
+    poolKeys = await raydium.liquidity.getAmmPoolKeys(poolId)
+  } else {
+    rpcData = await raydium.liquidity.getRpcPoolInfo(poolId)
+    if (!rpcData) throw new Error('pool info not found in devent')
+    const computeData = toAmmComputePoolInfo({ [poolId]: rpcData })
+    poolInfo = computeData[poolId]
+    const allKeys = await raydium.tradeV2.computePoolToPoolKeys({
+      pools: [computeData[poolId]],
+      ammRpcData: { [poolId]: rpcData },
+    })
+    poolKeys = allKeys[0] as AmmV4Keys
+  }
 
   if (poolInfo.mintA.address !== inputMint && poolInfo.mintB.address !== inputMint)
     throw new Error('input mint does not match pool')
-  if (!isValidAmm(poolInfo.programId)) throw new Error('target pool is not AMM pool')
 
   const baseIn = inputMint === poolInfo.mintA.address
   const [mintIn, mintOut] = baseIn ? [poolInfo.mintA, poolInfo.mintB] : [poolInfo.mintB, poolInfo.mintA]
-  const poolKeys = await raydium.liquidity.getAmmPoolKeys(poolId)
-
-  const res = await fetchMultipleInfo({
-    connection: raydium.connection,
-    poolKeysList: [poolKeys],
-    config: undefined,
-  })
-  const pool = res[0]
-
-  await raydium.liquidity.initLayout()
 
   const out = raydium.liquidity.computeAmountOut({
     poolInfo: {
       ...poolInfo,
-      baseReserve: pool.baseReserve,
-      quoteReserve: pool.quoteReserve,
-      status: pool.status.toNumber(),
+      baseReserve,
+      quoteReserve,
+      status,
       version: 4,
     },
     amountIn: new BN(amountIn),
@@ -59,6 +68,7 @@ export const swap = async () => {
 
   const { execute } = await raydium.liquidity.swap({
     poolInfo,
+    poolKeys,
     amountIn: new BN(amountIn),
     amountOut: out.minAmountOut, // out.amountOut means amount 'without' slippage
     fixedSide: 'in',
