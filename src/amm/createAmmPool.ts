4b519ce25631559e0c72b6ba169cab3a96d53f54
Cruz Shia <iamahan0720@gmail.com>
2024-07-29 12:13:54 +0800

    add create amm pool mints programid check

--- src/amm/createAmmPool.ts
+++ src/amm/createAmmPool.ts
@@ -7,6 +7,7 @@ import {
 } from '@raydium-io/raydium-sdk-v2'
 import { initSdk, txVersion } from '../config'
 import { PublicKey } from '@solana/web3.js'
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import BN from 'bn.js'
 
 export const createAmmPool = async () => {
@@ -22,6 +23,15 @@ export const createAmmPool = async () => {
 
   const baseMintInfo = await raydium.token.getTokenInfo(baseMint)
   const quoteMintInfo = await raydium.token.getTokenInfo(quoteMint)
+
+  if (
+    baseMintInfo.programId !== TOKEN_PROGRAM_ID.toBase58() ||
+    quoteMintInfo.programId! == TOKEN_PROGRAM_ID.toBase58()
+  ) {
+    throw new Error(
+      'amm pools with openbook market only support TOKEN_PROGRAM_ID mints, if you want to create pool with token-2022, please create cpmm pool instead'
+    )
+  }
 
   const { execute, extInfo } = await raydium.liquidity.createPoolV4({
     programId: AMM_V4,
