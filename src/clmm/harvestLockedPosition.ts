ffda9bb662a7371975fd13f88c1f937d444c8ad7
Cruz Shia <iamahan0720@gmail.com>
2024-10-25 18:04:56 +0800

    clmm position lock and harvest

---
+++ src/clmm/harvestLockedPosition.ts
@@ -0,0 +1,61 @@
+import {
+  ApiV3PoolInfoConcentratedItem,
+  CLMM_PROGRAM_ID,
+  DEVNET_PROGRAM_ID,
+  PositionInfoLayout,
+  printSimulate,
+  getPdaLockClPositionIdV2,
+  CLMM_LOCK_PROGRAM_ID,
+  LockClPositionLayoutV2,
+} from '@raydium-io/raydium-sdk-v2'
+import { PublicKey } from '@solana/web3.js'
+import { initSdk, txVersion } from '../config'
+import { BN } from 'bn.js'
+
+export const harvestLockedPosition = async () => {
+  const raydium = await initSdk({ loadToken: true })
+
+  const lockNftMint = new PublicKey('your locked position nft mint')
+  const lockPositionId = getPdaLockClPositionIdV2(CLMM_LOCK_PROGRAM_ID, lockNftMint).publicKey
+  const res = await raydium.connection.getAccountInfo(lockPositionId)
+  const lockData = LockClPositionLayoutV2.decode(res!.data)
+
+  /** code below can fetch all your locked clmm positions */
+  //   await raydium.account.fetchWalletTokenAccounts()
+  //   const possibleLockMints = raydium.account.tokenAccountRawInfos
+  //     .filter((a) => a.accountInfo.amount.eq(new BN(1)) && !raydium.token.tokenMap.has(a.accountInfo.mint.toBase58()))
+  //     .map((a) => a.accountInfo.mint)
+
+  //   const possibleLockPositionId = possibleLockMints.map(
+  //     (m) => getPdaLockClPositionIdV2(CLMM_LOCK_PROGRAM_ID, m).publicKey
+  //   )
+  //   const res = await raydium.connection.getMultipleAccountsInfo(possibleLockPositionId)
+  //   const allLockPositions = res
+  //     .map((r, idx) =>
+  //       r
+  //         ? {
+  //             ...LockClPositionLayoutV2.decode(r.data),
+  //             lockPositionId: possibleLockPositionId[idx],
+  //           }
+  //         : undefined
+  //     )
+  //     .filter(Boolean) as (ReturnType<typeof LockClPositionLayoutV2.decode> & { lockPositionId: PublicKey })[]
+  //   if (!allLockPositions.length) throw new Error('you do not have any lock positions')
+  //   const lockData = allLockPositions[0] // find out which lock position you want to harvest
+
+  const { execute, transaction } = await raydium.clmm.harvestLockPosition({
+    lockData,
+    txVersion,
+    // optional: set up priority fee here
+    // computeBudgetConfig: {
+    //     units: 600000,
+    //     microLamports: 100000,
+    //   },
+  })
+
+  //   const { txId } = await execute({})
+  //   console.log('harvested locked position :', { txId: `https://explorer.solana.com/tx/${txId}` })
+}
+
+/** uncomment code below to execute */
+// harvestLockedPosition()
---
+++ src/clmm/lockPosition.ts
@@ -0,0 +1,49 @@
+import {
+  ApiV3PoolInfoConcentratedItem,
+  CLMM_PROGRAM_ID,
+  DEVNET_PROGRAM_ID,
+  PositionInfoLayout,
+  getPdaPersonalPositionAddress,
+} from '@raydium-io/raydium-sdk-v2'
+import { PublicKey } from '@solana/web3.js'
+import { initSdk, txVersion } from '../config'
+
+export const lockPosition = async () => {
+  const raydium = await initSdk()
+
+  const positionNftMint = new PublicKey('your position nft mint')
+  const positionPubKey = getPdaPersonalPositionAddress(CLMM_PROGRAM_ID, positionNftMint).publicKey // devnet:  DEVNET_PROGRAM_ID.CLMM
+  const pos = await raydium.connection.getAccountInfo(positionPubKey)
+  const position = PositionInfoLayout.decode(pos!.data)
+
+  /** if you want to fetch all existed position to find which position you want */
+  //  const allPosition = await raydium.clmm.getOwnerPositionInfo({ programId: CLMM_PROGRAM_ID }) // devnet: DEVNET_PROGRAM_ID.CLMM
+
+  // RAY-USDC pool
+  // note: api doesn't support get devnet pool info
+  const positionPoolInfoList = (await raydium.api.fetchPoolById({
+    ids: position.poolId.toBase58(),
+  })) as ApiV3PoolInfoConcentratedItem[]
+
+  const poolInfo = positionPoolInfoList[0]
+  if (!poolInfo) throw new Error(`clmm pool ${position.poolId.toBase58()} not found`)
+
+  const { execute, transaction } = await raydium.clmm.lockPosition({
+    // programId:DEVNET_PROGRAM_ID.CLMM_LOCK_PROGRAM_ID, // devnet
+    // authProgramId:DEVNET_PROGRAM_ID.CLMM_LOCK_AUTH_ID, // devnet
+    // poolProgramId:  new PublicKey(poolInfo.programId),
+    ownerPosition: position,
+    txVersion,
+    // optional: set up priority fee here
+    // computeBudgetConfig: {
+    //   units: 600000,
+    //   microLamports: 100000000,
+    // },
+  })
+
+  const { txId } = await execute({})
+  console.log('position locked:', { txId: `https://explorer.solana.com/tx/${txId}` })
+}
+
+/** uncomment code below to execute */
+// lockPosition()
