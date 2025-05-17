4820a4ba7dd2dcf9df4cf1acc713feb1e359d013
Cruz Shia <iamahan0720@gmail.com>
2025-05-17 08:24:06 +0800

    update sdk and claimVesting

--- src/launchpad/claimVesting.ts
+++ src/launchpad/claimVesting.ts
@@ -1,21 +1,13 @@
 import { TxVersion, DEV_LAUNCHPAD_PROGRAM, printSimulate, LAUNCHPAD_PROGRAM } from '@raydium-io/raydium-sdk-v2'
 import { initSdk } from '../config'
 import { PublicKey } from '@solana/web3.js'
-import { NATIVE_MINT } from '@solana/spl-token'
 
 export const claimVesting = async () => {
   const raydium = await initSdk()
 
-  /** notice: every wallet only enable to create "1" platform config */
-  const { transaction, extInfo, execute } = await raydium.launchpad.claimPlatformFee({
+  const { transaction, extInfo, execute } = await raydium.launchpad.claimVesting({
     programId: LAUNCHPAD_PROGRAM, // devent: DEV_LAUNCHPAD_PROGRAM
-    platformId: new PublicKey('platform id'),
-    platformClaimFeeWallet: new PublicKey('vesting receiver'),
     poolId: new PublicKey('pool id'),
-
-    mintB: NATIVE_MINT,
-    vaultB: new PublicKey('pool vault B'),
-
     txVersion: TxVersion.V0,
     // computeBudgetConfig: {
     //   units: 600000,
