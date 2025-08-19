00e160e57dff9ae59ff44c290b4edd30ff73156d
Allen <allengeorge2400@gmail.com>
2025-08-19 13:26:16 +0000

    Fixed typo in utils.ts

--- src/amm/utils.ts
+++ src/amm/utils.ts
@@ -3,8 +3,8 @@ import { AMM_V4, AMM_STABLE, DEVNET_PROGRAM_ID } from '@raydium-io/raydium-sdk-v
 const VALID_PROGRAM_ID = new Set([
   AMM_V4.toBase58(),
   AMM_STABLE.toBase58(),
-  DEVNET_PROGRAM_ID.AmmV4.toBase58(),
-  DEVNET_PROGRAM_ID.AmmStable.toBase58(),
+  DEVNET_PROGRAM_ID.AMM_V4.toBase58(),
+  DEVNET_PROGRAM_ID.AMM_STABLE.toBase58(),
 ])
 
 export const isValidAmm = (id: string) => VALID_PROGRAM_ID.has(id)
